83664ee055f50d9be11c31f56242d5af0644afab
vacantthinker <littlehappyforsmile@outlook.com>
2023-03-30 15:34:01 +0800

    'update'

--- tmp.js
+++ tmp.js
@@ -1,5 +1,5 @@
+const {setupVueToElectron} = require('./util/index.electron.vue');
 
-const {setupVueToElectron} = require('@vacantthinker/util_electron_vue_js');
 setupVueToElectron(
     `youtube playlist download queue`,
     `queue-download-desktop`);
